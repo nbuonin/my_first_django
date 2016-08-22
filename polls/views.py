846be7b2e680fb9391b1f11e007b370f3f94cd86
Nick Buonincontri <nbuonin@gmail.com>
2016-08-21 22:32:01 -0400

    fixing comma

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,7 @@ def index(request):
     return HttpResponse("Hello, World!")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s.", % question_id)
+    return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
