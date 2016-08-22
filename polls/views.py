4fdbcb9d1c90a8e104a6179d9bb461d3a4b67749
Nick Buonincontri <nbuonin@gmail.com>
2016-08-21 22:33:21 -0400

    fixing syntax

--- polls/views.py
+++ polls/views.py
@@ -14,4 +14,4 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." question_id)
+    return HttpResponse("You're voting on question %s." % question_id)
